8f9df1b039868333612bcbb3421f0bf863f8dd45
CHARANCHERRY123456 <cherryiiit1234@gmail.com>
2025-05-25 16:35:52 +0530

    trigger redploy after env

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,6 +43,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Rate limiting
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
